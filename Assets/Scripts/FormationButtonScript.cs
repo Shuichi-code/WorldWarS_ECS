1c1815fa52fba19f515f96cba8b7e575bc981c14
Shuichi-code <53144657+Shuichi-code@users.noreply.github.com>
2021-02-23 17:32:53 +0800

    Attempted to use UIElements for the PiecePlacementUI. Did not succeed

--- Assets/Scripts/FormationButtonScript.cs
+++ Assets/Scripts/FormationButtonScript.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class FormationButtonScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void OnPress()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("Button has been pressed.");
     }
 }
---
+++ Assets/Scripts/UI/UIBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.UIElements.Runtime;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class UIBuilder : MonoBehaviour
+{
+    private Button deployButton;
+
+    private void OnEnable()
+    {
+        var rootVisualElement = GameObject.Find("UIBuilderManager").GetComponent<PanelRenderer>().visualTree;
+
+        deployButton = rootVisualElement.Q<Button>(name = "DeployButton");
+        deployButton.RegisterCallback<MouseUpEvent>(ev => ButtonPressed());
+    }
+
+    private void ButtonPressed()
+    {
+        Debug.Log("Deploy Button has been pressed!");
+    }
+}
