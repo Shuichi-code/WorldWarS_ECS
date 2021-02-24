8b65be62254db3482f954bc3f53e94854982debd
Shuichi-code <53144657+Shuichi-code@users.noreply.github.com>
2021-02-24 17:38:17 +0800

    Tried to implement UI Element. Still no success

--- Assets/Scripts/UI/UIBuilder.cs
+++ Assets/Scripts/UI/UIBuilder.cs
@@ -1,19 +1,70 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using Unity.UIElements.Runtime;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+using Unity.UIElements.Runtime;
+using Button = UnityEngine.UIElements.Button;
 
 public class UIBuilder : MonoBehaviour
 {
-    private Button deployButton;
+    private PanelRenderer panelRenderer;
+
+    private void Start()
+    {
+        panelRenderer = this.GetComponent<PanelRenderer>();
+        panelRenderer.postUxmlReload += OnloadUXML;
+    }
+
+    //call OnLoad of UXML in panelRenedere
+    IEnumerable<Object> OnloadUXML()
+    {
+
+        var root = panelRenderer.visualTree;
+
+        //Find & Subscribe Method onclick event On "Tap to Start" (Name in UXML is "Play" ) Button of Mainmenu Screen
+        var tapToStart = root.Q<Button>("DeployButton"); // find Play button in Uxml(visualTree)
+        if (tapToStart != null)
+        {
+            tapToStart.clicked += OnPlay; // subscribe event
+        }
+
+
+        //Find & Subscribe Method onclick event On "Exit" (Name in UXML is "Exit" ) Button of Mainmenu Screen
+        var exit = root.Q<Button>("ReturnButton");// find Exit button in Uxml(visualTree)
+        if (exit != null)
+        {
+            exit.clicked += OnExit; // subscribe event
+        }
+
+        return null;
+    }
+
+    // Call OnTap of Play button of MainmenuScreen
+    private void OnPlay()
+    {
+        Debug.Log("Clicked on DeployButton");
+    }
+
+
+    // Call OnTap of Exit button of MainmenuScreen
+    private void OnExit()
+    {
+        Debug.Log("Clicked on ReturnButton");
+    }
 
     private void OnEnable()
     {
-        var rootVisualElement = GameObject.Find("UIBuilderManager").GetComponent<PanelRenderer>().visualTree;
-
-        deployButton = rootVisualElement.Q<Button>(name = "DeployButton");
-        deployButton.RegisterCallback<MouseUpEvent>(ev => ButtonPressed());
+        if (panelRenderer != null)
+        {
+            Debug.Log("PanelRenderer exists!");
+        }
+        else
+        {
+            Debug.Log("PanelRenderer does not exist!");
+        }
+        //var deployButton = panelRenderer.visualTree.Q<Button>("DeployButton");
+        //deployButton.clickable.clickedWithEventInfo += (evt) => Debug.Log("Button has been pressed");
+        //deployButton.clickable.clicked += () => Debug.Log("Clicked!");
+        //deployButton.RegisterCallback<MouseUpEvent>(ev => ButtonPressed());
     }
 
     private void ButtonPressed()
